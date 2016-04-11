74454651e01bef8b0f585f27756d4ddb540b1827
hdse09 <hansy.solis@exelsolar.com>
2016-04-11 12:32:23 -0500

    Prueba 1

---
+++ EulerProjects/Problem1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProjects
+{
+    public class Problem1
+    {
+        public string Nombre {
+            get { return "Suma de multiplos de 3 y 5 anteriores a 1000"; }
+        }
+
+        public void GetAnswer()
+        {
+            Console.WriteLine("Empezando problema: "+Nombre);
+            int suma = 0;
+            int top = 15;
+            int i = 1;
+            var multiplostres = new ArrayList();
+            var multiploscinco = new ArrayList();
+            while (i*3 < top)
+            {
+                multiplostres.Add(i * 3);
+                i++;
+            }
+            i = 1;
+            while (i * 5 < top)
+            {
+                multiploscinco.Add(i * 5);
+                i++;
+            }
+            Console.WriteLine("Respuesta : " + suma);
+        }
+    }
+}
---
+++ EulerProjects/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerProjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Empezando Problemas");
+            Console.WriteLine("Tiempo Inicio "+DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt"));
+            Problem1 problem = new Problem1();
+            problem.GetAnswer();
+            Console.WriteLine("Tiempo Fin " + DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff tt"));
+            Console.ReadLine();
+        }
+    }
+}
